ee8d9ac714a310e701daf3667bae65d403a9251a
usamaslaam <105082672+usamaslaam@users.noreply.github.com>
2023-05-31 15:56:00 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+</head>
+<body>
+  
+  <?php
+    echo "Filler text is text that shares some characteristics of a real written text, but is random or otherwise generated. It may be used to display a sample of fonts, generate text for testing, or to spoof an e-mail spam filter. ";
+  ?>
+ 
+</body>
+</html>
